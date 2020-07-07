ea25e24e8d0f157ac5122d3963aeeec3a40b5fda
BalkanBoy19 <ndrekovic@outlook.de>
2020-07-07 14:26:45 +0200

    Verfeinerung

--- sketch.js
+++ sketch.js
@@ -5,12 +5,12 @@ function setup() {
   createCanvas(600, 200);
   background(100);
 
-  r = new Square("rot", 1, width/2, height-40);
-  b = new Square("blau", 1, width/2-80, height-(height-20));
+  r = new Square("rot", 1, width/2, height-40, 0.5);
+  b = new Square("blau", 1, width/2-80, height-(height-20), 1.25);
 
   fill(255, 0, 0);
   rect(r.left_pos, height-40, height-(height-40), height-(height-40));
- 
+  
   fill(0, 0, 255);
   rect(b.left_pos, height-20, height-(height-20), height-(height-20));
   
@@ -28,11 +28,17 @@ function draw() {  // besser mit einer Klasse + Konstruktor Square mit Attribute
    fill(0, 0, 255); // blau
    rect(b.left_pos, height-20, height-(height-20), height-(height-20));
 
-   b.left_pos += Math.pow(-1, b.richtung);
+   b.left_pos += (Math.pow(-1, b.richtung) * b.speed);
+   r.left_pos += (Math.pow(-1, r.richtung) * r.speed);
    
-   if ((b.left_pos == 0) || (b.left_pos + b.right_pos == r.left_pos)) {  // also sobald bei 0 angekommen, eleganteren Weg auswählen statt Bool Variable vor
+   if ((b.left_pos == 0) || (b.left_pos + b.right_pos >= r.left_pos)) {  // also sobald bei 0 angekommen, eleganteren Weg auswählen statt Bool Variable vor
     b.richtung = !b.richtung;  // um von rechts nach links zu wechseln und umgekehrt (wechselt von true zu false bzw. von 1 zu 0 und umgekehrt)
+       
     print("Stoooooß");
+   }
+   
+   if (r.left_pos == (b.left_pos + b.right_pos)) {
+    r.richtung = !r.richtung;
    }
    
  }
@@ -40,14 +46,15 @@ function draw() {  // besser mit einer Klasse + Konstruktor Square mit Attribute
 }
 
 function mousePressed() {
- start = true;
+ start = !start;
 }
 
-function Square(color, richtung, left_pos, right_pos) {
+function Square(color, richtung, left_pos, right_pos, speed) {
  this.color = color;
  this.richtung = richtung;  // 1 für rechts und 0 für links
  this.left_pos = left_pos;
  this.right_pos = right_pos;
+ this.speed = speed;
 }
 
 
